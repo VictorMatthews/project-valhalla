1bf97027e442ca6d28ca43e453abc36d3930ddf8
victormatthews <victormatthews@10.0.0.43>
2017-01-11 20:16:01 -0500

    01/11/2017 - Removed Stuff

--- gameHub/src/main/java/com/nfpenterprise/gameHub/constants/Paths.java
+++ gameHub/src/main/java/com/nfpenterprise/gameHub/constants/Paths.java
@@ -4,14 +4,6 @@ public enum Paths {
 	APPLICATION_ICON("file:src/main/resources/images/Project_Valhalla.png"),
 
 	MY_CHARACTER_DATA("file/Characters.xml"),
-	RACE_DATA("file/Races.xml"),
-	SUB_RACE_DATA("file/SubRaces.xml"),
-	CLASS_DATA("file/Classes.xml"),
-	BACKGROUND_DATA("file/Backgrounds.xml"),
-	PERSONALITY_TRAIT_DATA("file/PersonalityTraits.xml"),
-	IDEALS_DATA("file/Ideals.xml"),
-	BONDS_DATA("file/Bonds.xml"),
-	FLAWS_DATA("file/Flaws.xml"),
 
 	ROOT_LAYOUT_FXML("view/RootLayout.fxml"),
 	NEW_CHARACTER_MAIN_FXML("view/newCharacter/NewCharacterMain.fxml"),
